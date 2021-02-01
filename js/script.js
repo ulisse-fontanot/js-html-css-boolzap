2ebf4f3c36746acb024554ffba4cde9fd78c10bd
ulisse-fontanot <ulissef99@gmail.com>
2021-02-01 17:47:38 +0100

    contatti con js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: "#app",
+  data:{
+    contatti:[
+      "Dart Vader",
+      "Yoda",
+      "Chewbeca",
+      "Obi Wan Kenobi"
+    ],
+    src:[
+      "img/darth.jpg",
+      "img/yoda.jpeg",
+      "img/chewbe.png",
+      "img/obi.jpg"
+    ]
+  }
+})
